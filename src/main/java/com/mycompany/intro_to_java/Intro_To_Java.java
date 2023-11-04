59489c051a37dbce0f0a29573fafe0c09089e319
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-11-03 21:04:25 -0700

    Initial commit

---
+++ src/main/java/com/mycompany/intro_to_java/Intro_To_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.intro_to_java;
+
+/**
+ *
+ * @author SHEEVZ
+ */
+public class Intro_To_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
